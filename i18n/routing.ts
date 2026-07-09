b80632c55a84b6bdb83bd65d23c129f19b1398f9
ZILMERDEV <rocco.digiulio@zilmer.com.br>
2026-07-09 07:42:31 -0300

    Update routing.ts

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,21 +2,21 @@ import {defineRouting} from 'next-intl/routing';
 import {createNavigation} from 'next-intl/navigation';
 
 export const routing = defineRouting({
-locales: ['pt', 'en', 'es'],
-defaultLocale: 'pt',
-localePrefix: 'always',
-domains: [
-{
-domain: 'www.zilmer.com.br',
-defaultLocale: 'pt',
-locales: ['pt', 'en', 'es']
-},
-{
-domain: 'zilmer.com.br',
-defaultLocale: 'pt',
-locales: ['pt', 'en', 'es']
-}
-]
+  locales: ['pt', 'en', 'es'],
+  defaultLocale: 'pt',
+  localePrefix: 'always', // ISSO É OBRIGATÓRIO PARA CORRIGIR OS LINKS
+  domains: [
+    {
+      domain: 'www.zilmer.com.br',
+      defaultLocale: 'pt',
+      locales: ['pt', 'en', 'es']
+    },
+    {
+      domain: 'zilmer.com.br',
+      defaultLocale: 'pt',
+      locales: ['pt', 'en', 'es']
+    }
+  ]
 });
 
 export const {Link, redirect, usePathname, useRouter} = createNavigation(routing);
