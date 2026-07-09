05ad2bb96d71dd3aadcfa46d75af9c68c4cd3b79
ZILMERDEV <rocco.digiulio@zilmer.com.br>
2026-07-09 07:47:15 -0300

    Change Link import to use custom routing

--- app/[locale]/produtos/transformadores-instrumentos/page.tsx
+++ app/[locale]/produtos/transformadores-instrumentos/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import Link from 'next/link'
+import {Link} from '@/i18n/routing';
 import styles from './page.module.css'
 import { cdnUrl } from '@/lib/assets'
 import { tpInternosModels } from './tp-internos-data'
