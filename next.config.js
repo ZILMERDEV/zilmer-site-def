37590ec77609602a1b9a40aba82ee23910b90ae4
ZILMERDEV <rocco.digiulio@zilmer.com.br>
2026-07-09 07:43:57 -0300

    Format next.config.js for consistency

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
 const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-env: {
-NEXT_PUBLIC_CDN_URL: 'https://zilmer-static-assets-494934331329-us-east-2-an.s3.us-east-2.amazonaws.com',
-},
-images: {
-unoptimized: true,
-formats: ['image/avif', 'image/webp'],
-},
-trailingSlash: true,
-compress: true,
-poweredByHeader: false,
-reactStrictMode: true,
-compiler: {
-removeConsole: process.env.NODE_ENV === 'production',
-},
+  env: {
+    NEXT_PUBLIC_CDN_URL: 'https://zilmer-static-assets-494934331329-us-east-2-an.s3.us-east-2.amazonaws.com',
+  },
+  images: {
+    unoptimized: true,
+    formats: ['image/avif', 'image/webp'],
+  },
+  trailingSlash: true,
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
+
 module.exports = withNextIntl(nextConfig);
 
